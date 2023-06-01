b4d2e2e1e1f438534ec0f73e3f33f7803812ae5e
Deiby24 <deiby.8@hotmail.es>
2023-05-31 19:25:32 -0500

    start about me

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import './index.css'
 import  ReactDOM  from "react-dom/client";
 import { Navbar } from "./components/Navbar";
+import { AboutMe } from "./components/AboutMe";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <Navbar></Navbar>
+    <>
+        <Navbar></Navbar>
+        <AboutMe></AboutMe>
+    </>
 )
