d2ba584fe7894c3d95ac232b0051710e559306b1
Deiby24 <deiby.8@hotmail.es>
2023-06-27 18:34:12 -0500

    CorrectionsV2

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ export function AboutMe() {
     
     useEffect(() => {
         const typed = new Typed(AnimationAbout.current,{
-            strings: ['> Tengo 18 años, vivo en Antioquia/Medellín, estudie una técnica en desarrollo de software y debido a esto tengo un gran conocimiento en varios lenguajes de programación no obstante me gustaría adquirir mucho mas conocimiento del ya que tengo, puesto que me gusta aprender cosas nuevas, por otro lado me encantan los retos por que así demuestro mis habilidades y conocimientos. <br/>  Mis cualidades son : soy una persona autodidacta, creativa, eficiente, puntual, responsable, honesta, uno de mis fuertes es el trabajo en equipo ya que me gusta ayudar a los demás e incluso compartir mi conocimiento. Para mi el respeto es esencial por ello soy consciente de los tiempos de los demás y el mío, además me motiva demasiado realizar mis deberes de la mejor manera y que sean de excelente calidad.'],
+            strings: ['> Tengo 18 años, vivo en Antioquia/Medellín, estudie una técnica en desarrollo de software y debido a esto tengo un gran conocimiento en varios lenguajes de programación no obstante me gustaría adquirir mucho mas conocimiento del ya que tengo, puesto que me gusta aprender cosas nuevas, por otro lado me encantan los retos por que así demuestro mis habilidades y conocimientos. <br/><br/>  >Mis cualidades son : soy una persona autodidacta, creativa, eficiente, puntual, responsable, honesta, uno de mis fuertes es el trabajo en equipo ya que me gusta ayudar a los demás e incluso compartir mi conocimiento. Para mi el respeto es esencial por ello soy consciente de los tiempos de los demás y el mío, además me motiva demasiado realizar mis deberes de la mejor manera y que sean de excelente calidad.'],
             typeSpeed: 0,
         })
 
@@ -26,15 +26,7 @@ export function AboutMe() {
                     <h2>ABOUT ME</h2>
                 </div>
                 <div className="subContainerAbout">
-                    <div className="containerImgAbout">
-                        <div className="rounded-circle BackgrounAbout">
-                            <img alt="aboutimg" className="imgAboutMe" src={require('../img/AboutMe.jpg')}></img>
-                        </div>
-                    </div>
                     <div className="AboutMe">
-                        <h1 id="aboutName">
-                            Deiby Hernandez Perez
-                        </h1>
                         <div className="textAbout">
                             <span  ref={AnimationAbout}/>
                         </div>
